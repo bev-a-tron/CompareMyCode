9c5750fb6436e1f80e134a4c52b485ffe32ab8ae
bev-a-tron <beverly.a.lau@gmail.com>
2012-07-21 16:43:17 -0400

    put stri back

--- application.py
+++ application.py
@@ -1,6 +1,21 @@
 from flask import Flask,redirect,url_for,render_template,request,Markup
 
+
+stri=['''
+def sum_two(a+b):
+    if a==b: return 2*(a+b)
+    else return a+b''',
+'''
+def sleep_in(weekday, vacation):
+  if not weekday or vacation: return True
+  else: return False''']
+
+#stri=[]
+#f = open('data.txt')
+#for line in f:
+
 app=Flask(__name__)
+app.count=0
 
 @app.route('/',methods=['GET','POST'])
 def index():
@@ -11,17 +26,18 @@ def index():
 
 @app.route('/main')
 def main():
-    return redirect(url_for('item'))
+    if app.count<len(stri):
+        return redirect(url_for('item'))
+    else:
+        return render_template('end.html')
 
 @app.route('/item',methods=['GET'])
 def item():
-    codeinhtml=Markup(\
-    '''<pre>
-def sum_two(a+b):
-    if a==b: return 2*(a+b)
-    else return a+b
-    </pre>''')
-    return render_template('layout.html',num=1,code=codeinhtml)
+    codeinhtml=Markup('''<pre>
+%s
+</pre>'''%(stri[app.count]))
+    app.count+=1
+    return render_template('layout.html',num=app.count,code=codeinhtml)
 
 @app.route('/item',methods=['POST'])
 def item2():
